cc7542156ca6745c4c2cd2e8a89b2cb23734357d
Stanisaur <s.nikolov1000@gmail.com>
2026-02-04 22:06:15 +0000

    better timing

--- timing/global_time.c
+++ timing/global_time.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-#include <sys/timex.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
 int main() {
-    struct timex t = {0};
+    struct timespec ts;
+    // Pre-calculate the sleep duration: 100 microseconds (0.1ms)
+    struct timespec delay = {0, 100000}; 
+
     while(1) {
-        if (adjtimex(&t) != -1) {
+        if (clock_gettime(CLOCK_REALTIME, &ts) == 0) {
             FILE *f = fopen("/dev/shm/global_clock.tmp", "w");
             if (f) {
-                fprintf(f, "%ld.%09ld\n", t.time.tv_sec, t.time.tv_usec);
+                // %09ld ensures leading zeros for nanoseconds
+                fprintf(f, "%ld.%09ld\n", ts.tv_sec, ts.tv_nsec);
                 fclose(f);
+                // Atomic rename prevents readers from seeing a partial file
                 rename("/dev/shm/global_clock.tmp", "/dev/shm/global_clock");
             }
         }
-        nanosleep(&delay, NULL);; 
+        nanosleep(&delay, NULL);
     }
+    return 0;
 }
